654dd2f8e5cf55ef77105c4a1e8a0efff5ecf4d4
Sunil Shrestha <sunilshresthamail@gmail.com>
2022-04-17 16:11:49 +0545

    Updates

---
+++ precache-manifest.60df984bc3d3eeb7387cd2a44ae7f42e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d548dfb0cc989692787",
+    "url": "/Portfolio-react/static/css/main.25a839b9.chunk.css"
+  },
+  {
+    "revision": "9d548dfb0cc989692787",
+    "url": "/Portfolio-react/static/js/main.7b76fa6a.chunk.js"
+  },
+  {
+    "revision": "afaa1a312de30a8694a0",
+    "url": "/Portfolio-react/static/js/runtime~main.3837020e.js"
+  },
+  {
+    "revision": "809039c674d2cf9901fb",
+    "url": "/Portfolio-react/static/css/2.d7f92e94.chunk.css"
+  },
+  {
+    "revision": "809039c674d2cf9901fb",
+    "url": "/Portfolio-react/static/js/2.d826352a.chunk.js"
+  },
+  {
+    "revision": "0a1b294a3eabb02011e2d98a6655cde4",
+    "url": "/Portfolio-react/static/media/shrija.0a1b294a.jpg"
+  },
+  {
+    "revision": "fff40e3f3fb34de39f6a9e09f97eb7c9",
+    "url": "/Portfolio-react/static/media/SUNIL_Cv.fff40e3f.pdf"
+  },
+  {
+    "revision": "2250297660fb53226e80cf97bf06474b",
+    "url": "/Portfolio-react/static/media/profile.22502976.jpeg"
+  },
+  {
+    "revision": "8cc2ed13c9a3344123e0da92a13a3969",
+    "url": "/Portfolio-react/static/media/sunil.8cc2ed13.jpg"
+  },
+  {
+    "revision": "56b6cc51d1aa2fd7e42755b7d1fe72aa",
+    "url": "/Portfolio-react/static/media/medhouse.56b6cc51.jpg"
+  },
+  {
+    "revision": "9f62778af084279282225de7f2d6d3b2",
+    "url": "/Portfolio-react/static/media/frontend.9f62778a.jpg"
+  },
+  {
+    "revision": "663c1fb81d0b4151240adad085e22940",
+    "url": "/Portfolio-react/static/media/portfolio.663c1fb8.jpg"
+  },
+  {
+    "revision": "72bcfa7393017fddbdc47d78634befdb",
+    "url": "/Portfolio-react/static/media/astha.72bcfa73.jpg"
+  },
+  {
+    "revision": "6e3418d3d8f81628085ffab8063b9075",
+    "url": "/Portfolio-react/index.html"
+  }
+];
